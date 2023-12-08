d44e6d9b7a244b2d67db0fb7cacac2a600d9cafc
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-08 15:53:15 -0500

    Create division method

--- src/calculator.js
+++ src/calculator.js
@@ -10,7 +10,14 @@ function calculator() {
       throw new Error("subtract takes two numeric arguments");
     return a - b;
   }
-  return { sum, subtract };
+
+  function divide(a, b) {
+    if (arguments.length !== 2)
+      throw new Error("divide takes two numeric arguments");
+    if (b === 0) throw new Error("cannot divide by zero");
+    return a / b;
+  }
+  return { sum, subtract, divide };
 }
 
 module.exports = calculator;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -13,6 +13,10 @@ it("sums a pos and neg number correctly", () => {
   expect(calculator().sum(-5, 7)).toBe(2);
 });
 
+it("sums two floats correctly", () => {
+  expect(calculator().sum(0.7, 0.333)).toBeCloseTo(1.033);
+});
+
 it("throws an error with too few or too many arguments in sum", () => {
   function noSumArguments() {
     calculator().sum();
@@ -24,9 +28,9 @@ it("throws an error with too few or too many arguments in sum", () => {
   function threeSumArguments() {
     calculator().sum(1, 2, 3);
   }
-  expect(noSumArguments).toThrow();
-  expect(oneSumArgument).toThrow();
-  expect(threeSumArguments).toThrow();
+  expect(noSumArguments).toThrow("sum takes two numeric arguments");
+  expect(oneSumArgument).toThrow("sum takes two numeric arguments");
+  expect(threeSumArguments).toThrow("sum takes two numeric arguments");
 });
 
 // Subtract Tests
@@ -42,6 +46,10 @@ it("subtracts a pos and neg number correctly", () => {
   expect(calculator().subtract(-5, 7)).toBe(-12);
 });
 
+it("subtracts two floats correctly", () => {
+  expect(calculator().subtract(0.7, 0.333)).toBeCloseTo(0.367);
+});
+
 it("throws an error with too few or too many arguments in subtract", () => {
   function noSubtractArguments() {
     calculator().subtract();
@@ -53,9 +61,45 @@ it("throws an error with too few or too many arguments in subtract", () => {
   function threeSubtractArguments() {
     calculator().subtract(1, 2, 3);
   }
-  expect(noSubtractArguments).toThrow();
-  expect(oneSubtractArgument).toThrow();
-  expect(threeSubtractArguments).toThrow();
+  expect(noSubtractArguments).toThrow("subtract takes two numeric arguments");
+  expect(oneSubtractArgument).toThrow("subtract takes two numeric arguments");
+  expect(threeSubtractArguments).toThrow(
+    "subtract takes two numeric arguments"
+  );
 });
 
 // Divide Tests
+it("divides two pos numbers correctly", () => {
+  expect(calculator().divide(5, 7)).toBe(5 / 7);
+});
+
+it("divides two neg numbers correctly", () => {
+  expect(calculator().divide(-5, -8)).toBe(-5 / -8);
+});
+
+it("divides a pos and neg number correctly", () => {
+  expect(calculator().divide(-5, 7)).toBe(-5 / 7);
+});
+
+it("throws an error when dividing by zero", () => {
+  function divideByZero() {
+    calculator().divide(5, 0);
+  }
+  expect(divideByZero).toThrow("cannot divide by zero");
+});
+
+it("throws an error with too few or too many arguments in divide", () => {
+  function noSubtractArguments() {
+    calculator().divide();
+  }
+  function oneSubtractArgument() {
+    calculator().divide(1);
+  }
+
+  function threeSubtractArguments() {
+    calculator().divide(1, 2, 3);
+  }
+  expect(noSubtractArguments).toThrow("divide takes two numeric arguments");
+  expect(oneSubtractArgument).toThrow("divide takes two numeric arguments");
+  expect(threeSubtractArguments).toThrow("divide takes two numeric arguments");
+});
