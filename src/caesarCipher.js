029977de788322c4e684ea00b14e47d2fa264fa6
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-08 18:34:25 -0500

    Implement cipher for positive keys

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,11 +1,24 @@
-// Use UTF-16 feff0020 to feff007e
+// Alphabet is from character code 32 (space) to 126 (tilde)
 
-function caesarCipher(string) {
-  if (arguments.length !== 1)
-    throw new Error("function takes only one argument");
+function encodeCharacter(char, key) {
+  const hexCode = char.charCodeAt(0);
+  const hexDiff = hexCode - 32;
+  const shiftedHexDiff = (hexDiff + key) % 95;
+  const shiftedHexCode = shiftedHexDiff + 32;
+  return String.fromCharCode(shiftedHexCode);
+}
+
+function caesarCipher(string, key) {
+  if (arguments.length !== 2) throw new Error("function takes two arguments");
   if (typeof string !== "string")
-    throw new Error("function take only string arguments");
-  return "";
+    throw new Error("first argument must be a string");
+  if (!Number.isInteger(key))
+    throw new Error("second argument must be an integer");
+  if (string === "") return "";
+  const encodedCharacters = string
+    .split("")
+    .map((char) => encodeCharacter(char, key));
+  return encodedCharacters.join("");
 }
 
 module.exports = caesarCipher;
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,23 +1,42 @@
 const caesarCipher = require("./caesarCipher");
 
 it("returns an empty string when passed one", () => {
-  expect(caesarCipher("")).toBe("");
+  expect(caesarCipher("", 1)).toBe("");
 });
 
 it("throws an error if too many or few arguments are passed", () => {
   function noArguments() {
     caesarCipher();
   }
-  function twoArguments() {
-    caesarCipher("a", "b");
+  function threeArguments() {
+    caesarCipher("a", "b", "c");
   }
-  expect(noArguments).toThrow("function takes only one argument");
-  expect(twoArguments).toThrow("function takes only one argument");
+  expect(noArguments).toThrow("function takes two arguments");
+  expect(threeArguments).toThrow("function takes two arguments");
 });
 
-it("throws an error if a non-string argument is passed", () => {
+it("throws an error if first argument is not a string", () => {
   function nonStringArgument() {
-    caesarCipher(1);
+    caesarCipher(1, 2);
   }
-  expect(nonStringArgument).toThrow("function take only string arguments");
+  expect(nonStringArgument).toThrow("first argument must be a string");
+});
+
+it("throws an error if second argument is not an integer", () => {
+  function nonIntegerArgument() {
+    caesarCipher("a", "b");
+  }
+  expect(nonIntegerArgument).toThrow("second argument must be an integer");
+});
+
+it("encodes a single character with a positive key", () => {
+  expect(caesarCipher("a", 2)).toBe("c");
+});
+
+it("wraps back to start of alphabet from the end", () => {
+  expect(caesarCipher("~", 1)).toBe(" ");
+});
+
+it("encodes a multi character string with a positive key", () => {
+  expect(caesarCipher("abc", 2)).toBe("cde");
 });
