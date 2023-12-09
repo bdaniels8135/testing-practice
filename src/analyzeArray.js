d7c788eac7d47595ce523f212ca24715cc1750ad
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-08 22:40:20 -0500

    Implement and test analyzeArray logic

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,5 +1,20 @@
 function analyzeArray(arr) {
-  return {};
+  if (arguments.length !== 1)
+    throw new Error("function requires an array of numbers to be passed");
+  if (!Array.isArray(arr))
+    throw new Error("function requires an array of numbers to be passed");
+  if (arr.length === 0)
+    throw new Error("function requires an array of numbers to be passed");
+  const length = arr.length;
+  const sum = arr.reduce((acc, curr) => {
+    if (!Number.isFinite(curr))
+      throw new Error("function requires an array of numbers to be passed");
+    return acc + curr;
+  }, 0);
+  const average = sum / length;
+  const min = arr.reduce((acc, curr) => (curr < acc ? curr : acc));
+  const max = arr.reduce((acc, curr) => (curr > acc ? curr : acc));
+  return { length, min, max, average };
 }
 
 module.exports = analyzeArray;
--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -14,3 +14,58 @@ it("throws an error when too many or too few arguments are passed", () => {
     "function requires an array of numbers to be passed"
   );
 });
+
+it("throws an error if argument is not an array", () => {
+  function nonArrayArgument() {
+    analyzeArray("a");
+  }
+  expect(nonArrayArgument).toThrow(
+    "function requires an array of numbers to be passed"
+  );
+});
+
+it("throw an error if array is empty", () => {
+  function emptyArray() {
+    analyzeArray([]);
+  }
+  expect(emptyArray).toThrow(
+    "function requires an array of numbers to be passed"
+  );
+});
+
+it("returns the correct length", () => {
+  expect(analyzeArray([1, 2, 3]).length).toBe(3);
+});
+
+it("returns the correct average for all positive numbers", () => {
+  expect(analyzeArray([1, 2, 3]).average).toBe(2);
+});
+
+it("returns the correct average for pos and neg numbers", () => {
+  expect(analyzeArray([-100, 100, 0]).average).toBe(0);
+});
+
+it("returns the correct min for all positive numbers", () => {
+  expect(analyzeArray([1, 2, 3]).min).toBe(1);
+});
+
+it("returns the correct min for pos and neg numbers", () => {
+  expect(analyzeArray([-100, 100, 0]).min).toBe(-100);
+});
+
+it("returns the correct max for all positive numbers", () => {
+  expect(analyzeArray([1, 2, 3]).max).toBe(3);
+});
+
+it("returns the correct max for pos and neg numbers", () => {
+  expect(analyzeArray([-100, 100, 0]).max).toBe(100);
+});
+
+it("throws an error if array has non-number elements", () => {
+  function nonNumberElements() {
+    analyzeArray([1, 2, "a", true, [1]]);
+  }
+  expect(nonNumberElements).toThrow(
+    "function requires an array of numbers to be passed"
+  );
+});
